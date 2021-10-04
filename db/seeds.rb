95316ffc1fe364a2bbbafdd1665fec017939ff99
Harold Torres Marino <haroldtm55@gmail.com>
2021-10-04 23:36:47 +1100

    Created CRUD files and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# require 'rest-client'
+# require 'json'
+def get_pokemon(id)
+  response = RestClient.get("https://pokeapi.co/api/v2/pokemon/#{id}")
+  JSON.parse(response)
+end
+
+def get_type(id)
+  response = RestClient.get("https://pokeapi.co/api/v2/type/#{id}")
+  JSON.parse(response)
+end
+
+#Generate pokemons
+(1..151).to_a.each do |id|
+  Pokemon.create(name: get_pokemon(id)["name"], image: get_pokemon(id)["sprites"]["front_default"])
+end
+
+#Generate types
+(1..18).to_a.each do |id|
+  Type.create(name: get_type(id)["name"])
+end
+
+#Randomnly assign types to pokemons (pokemon_types)
+Pokemon.all.each do |pokemon|
+  3.times do
+    PokemonType.find_or_create_by(pokemon_id: pokemon.id, type_id: (1..18).to_a.sample)
+  end
+end
